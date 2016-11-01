01678f18bc1cb75a472eb6da3bf514b69f095ed9
FacundoCuba <facundogcuba@gmail.com>
2016-11-01 18:28:11 -0300

    Se creo una funcion para la creacion de los niveles predeterminados en niveles.py y su llamado en modoPredeterminado.py

--- modoPredeterminado.py
+++ modoPredeterminado.py
@@ -6,7 +6,8 @@ def predeterminado():
         print("")
         print("Turnos restantes: ", turnos)
         print("")
-        for fila in enumerate(niveles.nivel1):
+        print(niveles.columnas)
+        for fila in enumerate(niveles.nivelesPredeterminados[0]):
             print(str(fila))
         print("")
         print("Ingrese su movimiento (de A1 a E5), Reinicie el nivel (R) o Regrese al menu principal (M): ")
