75444a8711b0b485602d45a9afe88b8038cdaeb2
FacundoCuba <facundogcuba@gmail.com>
2016-11-06 19:55:49 -0300

    En niveles.py se crearon las funciones getNivelAleatorio.py y cargarNivelAleatorio.py y se mejoro la funcion nivelAleatorio.py para crear todos los niveles aleatorios en funcion del input del usuario en modoAleatorio.py.

--- niveles.py
+++ niveles.py
@@ -13,25 +13,39 @@ def cargarNivelesPredeterminados():
 def getNivelPredeterminado(numeroNivel):
     numeroNivelesValidos = (1, 2, 3, 4, 5)
     if numeroNivel in numeroNivelesValidos:
-        print(columnas)
-        return nivelesPredeterminados[numeroNivel - 1]
+        print(columnas[:5])
+        return cargarNivelesPredeterminados()[numeroNivel - 1]
     else:
         print("Nivel no disponible...")
         print("")
         menu.mostrarMenu()
 
 def nivelAleatorio(numeroDeDimension):
-    columnas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-    columnas = columnas[:(numeroDeDimension)]
-    nivel = [columnas]
-    for filas in range(numeroDeDimension):
+    nivel = []
+    for fila in range(numeroDeDimension):
         opciones = ["0", "."]
         fila = []
-        for columnas in range(numeroDeDimension):
+        for columna in range(numeroDeDimension):
             fila.append(random.choice(opciones))
         nivel.append(fila)
     return nivel
 
+def cargarNivelesAleatorios(numeroDeDimension):
+    niveles = []
+    for nivel in range(5):
+        niveles.append(nivelAleatorio(numeroDeDimension))
+    return niveles
+
+def getNivelAleatorio(numeroNivel,numeroDeDimension):
+    numeroNivelesValidos = (1, 2, 3, 4, 5)
+    if numeroNivel in numeroNivelesValidos:
+        print(columnas[:(numeroDeDimension)])
+        return cargarNivelesAleatorios(numeroDeDimension)[numeroNivel - 1]
+    else:
+        print("Nivel no disponible...")
+        print("")
+        menu.mostrarMenu()
+
 nivelesPredeterminados = cargarNivelesPredeterminados()
-columnas = ("A", "B", "C", "D", "E")
+columnas = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
 filas = ("1", "2", "3", "4", "5")
