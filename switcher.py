87dabc0f2d83b08b70730f1fa547e765faa184c1
FacundoCuba <facundogcuba@gmail.com>
2016-11-15 20:00:40 -0300

    Se agrego un #TODO con la estructura para la funcion que cambia las luces.

--- switcher.py
+++ switcher.py
@@ -4,7 +4,7 @@ import juegoModoPredeterminado
 def traduccionDeCoordenadas(movimientoRealizado):
     fila = niveles.filas.index(movimientoRealizado[1])
     columna = niveles.columnas.index(movimientoRealizado[0])
-    return (fila, columna)
+    return fila, columna
 
 def buscaVecinos(movimientoRealizado):
     tuplaDelMovimiento = traduccionDeCoordenadas(movimientoRealizado)
@@ -17,3 +17,16 @@ def buscaVecinos(movimientoRealizado):
         vecinos.append((tuplaDelMovimiento[0], tuplaDelMovimiento[1] + 1))
     if 0 <= tuplaDelMovimiento[0] + 1 < 5:
         vecinos.append((tuplaDelMovimiento[0] + 1, tuplaDelMovimiento[1]))
+    return vecinos
+
+#TODO mecanismo de swich para las luces
+ '''''''''
+def swich(movimientoRealizado):
+    nivelActual = juegoModoPredeterminado.nivelActual
+    lucesACambiar = buscaVecinos(movimientoRealizado)
+    for tupla in lucesACambiar:
+        if nivelActual[tupla[0]][tupla[1]] == "0":
+            nivelActual[tupla[0]][tupla[1]].replace(".")
+        elif nivelActual[tupla[0]][tupla[1]] == ".":
+            nivelActual[tupla[0]][tupla[1]].replace("0")
+'''''''''''
